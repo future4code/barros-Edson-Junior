10fac9dd256040fcd8cf8215b0b2871f23051f7c
Edson Almeida da Silva Junior <ed.junior@ymail.com>
2022-04-12 21:59:31 -0300

    arquivo em html e js

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,61 @@
+//---Exercícios de interpretação---//
+//Exercício 1
+/*Resposta - 10 10,5
+
+Exercício 2
+Resposta - 10,10,10
+
+Exercício 3
+Resposta - para a variavel "let p" eu usaria "let horasTrabalhadas"
+Para a variavel "let t" eu usaria "let salarioDiario"*/
+
+//---Exercício de escrita---//
+
+/*let nomeCompleto = prompt ("Qual o seu nome?")
+
+let idadeCompleta = prompt ("Qual a sua idade?")*/
+
+//console.log(typeof nome, typeof idade)
+
+// acredito que os tipos ficaram como string porque no código os valores são strings. Talvez eu devesse fazer uma conversão de tipos?!
+
+
+//let idadeNumero = number(idade)
+
+//console.log(typeof nome, typeof idadeNumero)
+
+//console.log("Olá" ,nomeCompleto,", você tem" ,idadeCompleta, "anos.")
+
+/*let agua = prompt("Você bebeu água hoje?")
+let dormir = prompt("Você dormiu bem hoje?")
+let moqueca = prompt("Você gosta de moqueca de peixe?")
+
+console.log("Você bebeu água hoje?", agua)
+console.log("Você dormiu bem hoje?", dormir)
+console.log("Você gosta de moqueca de peixe?", moqueca)*/
+
+/*let a = 10
+let b = 25
+
+c=a
+a=b
+b=c
+
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)*/
+
+//tentei fazer o desafio, mas não fui bem sucedido.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
