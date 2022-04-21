dfd462ddf2beb1e244ca2d6b7b33cb44daf279b7
Edson Almeida da Silva Junior <ed.junior@ymail.com>
2022-04-21 10:13:03 -0300

    exercício strings-arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,86 @@
+// -----Exercícios de Interpretação de Código-----
+
+//Exercício 1
+
+//a. undefined
+
+//b. null
+
+//c. 11
+
+//d. 3
+
+//e. [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+//f. 9
+
+
+//Exercício 2
+
+// SUBI NUM ÔNIBUS EM MIRROCOS
+
+
+
+//Exercício de Escrita de Código
+
+//Exercício 1
+/*
+let emailDoUsuario = prompt("Qual é o seu email?")
+
+let nomeDoUsuario = prompt("Qual é o seu nome?")
+
+console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem vinde, ${nomeDoUsuario}!`)
+*/
+//Exercício 2
+
+/*
+let array = ["lasanha", 'pizza', 'macarrão', 'bolo', 'empadão']
+
+let i = 0
+
+array[i+1]
+
+console.log(array)
+
+console.log(`Essas são as minhas comidas preferidas:`)
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+console.log(array[3])
+console.log(array[4])
+
+let suaComidaFavorita = prompt("qual a sua comida favorita?")
+
+
+
+array.splice(1,1)
+
+array=['lasanha', suaComidaFavorita, 'macarrão', 'bolo', 'empadão']
+
+console.log(array)
+*/
+
+//Exercício 3
+/*
+let listaDeTarefas = []
+
+let tarefa1 = prompt("Qual a sua primeira tarefa do dia?")
+
+let tarefa2 = prompt("Qual a sua segunda tarefa do dia?")
+
+let tarefa3 = prompt("Qual a sua terceira tarefa do dia?")
+
+listaDeTarefas.push(tarefa1, tarefa2, tarefa3)
+
+console.log(listaDeTarefas)
+
+let tarefaRealizada = prompt('Qual o índice da tarefa que você concluiu? 0, 1 ou 2?')
+
+tarefaRealizada=Number(tarefaRealizada)
+
+listaDeTarefas.splice(tarefaRealizada, 2)
+
+
+console.log(listaDeTarefas)
+
+*/
